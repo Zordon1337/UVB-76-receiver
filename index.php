4decd16bc374d7650749fd1092c0836cd0dd8b55
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-07-10 23:44:57 +0200

    first and probably last commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<h1>Thanks to printf.cc for making this as sound stream (idk how to explain this)</h1>
+<h1>Local UVB-76 receiver</h1>
+
+ 
+ <audio controls>
+  <source src="http://streams.printf.cc:8000/buzzer.ogg" type="audio/ogg">
+</audio> 
+
+<h1>this is how probably it works</h1>
+<p>
+printf.cc streams sound from buzzer as .ogg file (webbrowser can only play .ogg or .mp3 file)
